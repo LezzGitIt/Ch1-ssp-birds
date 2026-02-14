1db2f8381c71b1ff3bc7efe677f02b8aed611afc
Aaron Skinner <skinnerayayron93@gmail.com>
2026-02-13 17:48:32 -0800

    Code to plot differences in fit statistic (chi-squared, freeman-tukey) by site and replicate

--- Scripts/spOccupancy_ms.R
+++ Scripts/spOccupancy_ms.R
@@ -11,6 +11,8 @@ library(broom.mixed)
 library(ggpubr)
 library(cowplot)
 library(conflicted)
+library(posterior)
+library(ggdist)
 ggplot2::theme_set(theme_cowplot())
 conflicts_prefer(dplyr::select)
 conflicts_prefer(dplyr::filter)
@@ -22,6 +24,7 @@ conflicts_prefer(dplyr::filter)
 # Create space by removing old models
 rm(list = ls()[!(ls() %in% c())])
 msOcc_l <- readRDS("Rdata/msOcc_l_season.rds")
+str(msOcc_l)
 
 ## Load data
 Wrangling_repo <- "../Ssp-bird-data-wrangling/"
@@ -37,39 +40,51 @@ Pc_locs_dc_sf <- st_read(
 source("/Users/aaronskinner/Library/CloudStorage/OneDrive-UBC/Grad_School/Rcookbook/Themes_funs.R")
 source("Scripts/spOcc_fns.R")
 
-# NOTES:
-# 1) Requires unique spatial coordinates, so I added a small jitter (.001)
-# 2) Does not allow NAs in occurrence (site) covs
+Forest <- TRUE
 
 # Row identifier ----------------------------------------------------------
 #NOTE:: The unique row identifier  [e.g. ID x Year] is critical , this defines what a row is in your dataframes and how many rows each dataframe will have 
 Row_identifier <- c("Id_muestreo", "Ano_grp", "Season") #, "Season"
 
 # Non-spatial -----------------------------------------------------------
+stop()
 occ.formula <- as.formula("~ Ecoregion + Habitat + Ano1 + Canopy_cover + Tot_prec + te + ssp + (1 | Id_group_no_dc)") 
-det.formula <- as.formula("~ Julian_day + Pc_start + Nombre_institucion + Forest_bin") 
+det.formula <- as.formula("~ Forest_bin + Pc_start + Nombre_institucion + Prob_breed")
 
 # >Run mod ----------------------------------------------------------------
-stop()
+n.samples <- 8000
+n.chains <- 3
+
+# We throw away much of the MCMC work 
+n.burn <- 5000
+n.thin <- 8
+
+Samples_per_chain <- n.samples - n.burn
+Keep_per_chain <- Samples_per_chain / n.thin 
+
+# This is how many samples we end up with in the end
+Tot_samples <- Keep_per_chain * n.chains
+Tot_samples
+
 start <- Sys.time()
-ms_160_ns <- lfMsPGOcc(
+ms_100_ns <- lfMsPGOcc(
   occ.formula = occ.formula, 
   det.formula = det.formula, 
   data = msOcc_l, 
-  n.samples = 5000,
+  n.samples = 8000,
   n.factors = 7,
   #inits = inits, 
   #priors = priors, 
   #tuning = tuning, 
   verbose = TRUE, 
   n.report = 100, 
-  n.burn = 3000, 
-  n.thin = 2, 
+  n.burn = 5000, 
+  n.thin = 8, 
   n.chains = 3
 )
 end <- Sys.time()
 end - start
-Occ_mod <- ms_117_ns
+Occ_mod <- ms_100_ns
 
 # Expected log pointwise predictive density (elpd), the effective number of parameters (pD), waic is the sum of the waic values for each species 
 waic_spp <- waicOcc(Occ_mod) 
@@ -138,13 +153,10 @@ parms_df %>% filter(parameter == "Beta") %>%
   filter(str_detect(term, "Habitat")) %>%
   plot_parm_estimates(ms = TRUE)
 
-library(posterior)
-library(ggdist)
 draws_beta <- as_draws_df(Occ_mod$beta.comm.samples) %>% 
   pivot_longer(cols = everything(), names_to = "param", values_to = "value")
 draws_alpha <- as_draws_df(Occ_mod$alpha.comm.samples) %>% 
   pivot_longer(cols = everything(), names_to = "param", values_to = "value")
-
 
 # Only 1. chain? 
 draws_beta %>% filter(param == ".chain") %>% 
@@ -176,8 +188,13 @@ ggsave("Figures/alpha_post_8fac.png", bg = "white") #beta
 # Can play with bayesplot::mcmc_dens() function as well, or as.data.frame() to get posterior draws 
 
 # Spatial -----------------------------------------------------------
-occ.formula <- as.formula("~ Ecoregion + Habitat + Ano1 + Canopy_cover + Tot_prec + te + ssp + (1 | Id_group_no_dc)") 
-det.formula <- as.formula("~ Forest_bin + Pc_start + Nombre_institucion + Prob_breed") 
+if(Forest){ # Forest_typ, Departamento
+  occ.formula <- as.formula("~ Canopy_height_m + Ano1 + I(Ano1^2) + Tot_prec + Elev + (1 | Id_group_no_dc) + (1 | Ecoregion)") # te + ssp + Canopy_cover 
+  det.formula <- as.formula("~ Pc_start + I(Pc_start^2) + Prob_breed + (1 | Collector_team_num)") #Julian_day + I(Julian_day^2)  
+} else{
+  occ.formula <- as.formula("~ Habitat + te + ssp + (1 | Id_group_no_dc)") # Ecoregion
+  det.formula <- as.formula("~ Forest_bin + Pc_start + I(Pc_start^2) + Julian_day + I(Julian_day^2) + (1 | Collector_team_num)") # Forest_bin +
+}
 
 ## Priors on spatial decay parameter (phi)
 dist_mat <- st_distance(Pc_locs_dc_sf)
@@ -197,9 +214,8 @@ priors <- list(beta.normal = list(mean = 0, var = 2.72),
                sigma.sq.unif = c(.5, 4), #c(.25, 2)
                phi.unif = list(lower, upper))
 
-
 ## Initial values
-n.factors <- 2
+n.factors <- 4
 # Number of species
 N <- nrow(msOcc_l$y)
 # Initiate all lambda initial values to 0. 
@@ -224,7 +240,7 @@ Samples_per_chain <- n.batch * batch.length # Total number of samples
 
 # We throw away much of the MCMC work 
 n.burn <- 6000 
-n.thin <- 4
+n.thin <- 8
 
 # Total samples per chain - burn in per chain
 Keep_per_chain <- (Samples_per_chain - n.burn) / n.thin 
@@ -232,9 +248,9 @@ Keep_per_chain <- (Samples_per_chain - n.burn) / n.thin
 Tot_samples <- Keep_per_chain * n.chains
 Tot_samples
 
-# dim(msOcc_l$y)
+dim(msOcc_l$y)
 start <- Sys.time()
-ms_14_season <- svcMsPGOcc(
+ms_37 <- svcMsPGOcc(
   occ.formula = occ.formula, 
   det.formula = det.formula, 
   data = msOcc_l, 
@@ -260,7 +276,7 @@ ms_14_season <- svcMsPGOcc(
 )
 end <- Sys.time()
 end - start
-Occ_mod <- ms_14_season
+Occ_mod <- ms_37
 
 # Expected log pointwise predictive density (elpd), the effective number of parameters (pD), waic is the sum of the waic values for each species 
 waic_spp <- waicOcc(Occ_mod) 
@@ -289,22 +305,22 @@ Prob_parms %>% tabyl(parameter, term)
 round(nrow(Prob_parms) / nrow(parms_df), 2) # 12%
 
 # >>Goodness of fit ------------------------------------------------------
-# Run custom functions to extract GOF 
-# These ppc don't fit unless you use n.thin
-ms_14_ppc <- ms_14_season %>%  
-  run_ppc() #%>% 
-gof_tbl_14 <- ms_14_ppc %>% extract_gof()
-
 ## Posterior predictive check for each species
-ppc_spp <- map(ms_14_ppc, calc_bayes_p_ms) %>% list_rbind() 
-# Plotting
+# Run custom functions to extract GOF 
+ms_ppc <- Occ_mod %>%  
+  run_ppc()
+ppc_spp <- map(ms_ppc, calc_bayes_p_ms) %>% list_rbind() 
+
+# Prep plotting
 ppc_spp_plot <- ppc_spp %>% mutate(group_fit = paste(group, fit_stat, sep = ", "))
+ppc_spp_plot %>% summarize(Comm_bayes_p = mean(bayes_p), .by = group_fit)
 # Subset species that 'pass / fail' the GOF test
 Spp_gof_fail <- ppc_spp_plot %>% filter(bayes_p < 0.1 | bayes_p > 0.9) 
-Spp_gof_pass <- ppc_spp_plot %>% filter(bayes_p > 0.1 | bayes_p < 0.9) 
+Spp_gof_pass <- ppc_spp_plot %>% filter(bayes_p > 0.1 & bayes_p < 0.9) 
 
 # Posterior density plot 
-ppc_spp_plot %>% ggplot(aes(x = bayes_p, color = group_fit)) + 
+ppc_spp_plot %>% 
+  ggplot(aes(x = bayes_p, color = group_fit)) + 
   geom_density() +
   theme(legend.position = "top")
 
@@ -320,10 +336,97 @@ ppc_spp_plot %>% ggplot(aes(y = group_fit, x = bayes_p)) +
   geom_vline(xintercept = 0.1, linetype = "dashed", alpha = .3, color = "red") +
   labs(x = "Bayesian P", y = NULL)
 
+# Examine the percent of GOF tests that 'fail'
+nrow(Spp_gof_fail) / nrow(ppc_spp) 
+
 # Identify the problematic species in terms of goodness of fit 
 Spp_gof_fail %>% count(Species, sort = T)
-# Examine the percent of GOF tests that 'fail'
-nrow(Spp_gof_fail) / nrow(ppc_spp) # 50%
+
+# >>>Plot by site / rep ---------------------------------------------------
+# Take the difference between the models prediction (replicate) and the actual data
+get_ppc_diff <- function(ms_ppc_obj, q = 3){
+  ms_ppc_obj$fit.y.rep.group.quants[q,,] - 
+    ms_ppc_obj$fit.y.group.quants[q,,]
+}
+ms_ppc[[2]]$fit.y.rep.group.quants[3,,]
+ms_ppc[[2]]$fit.y.group.quants[3,,]
+
+diff_q50_site <- get_ppc_diff(ms_ppc[[1]], q = 3)
+diff_q50_rep <- get_ppc_diff(ms_ppc[[2]], q = 3)
+
+# Turn into tibble and pivot_longer for plotting
+ppc_to_tbl <- function(diff_mat, index_name, index_n){
+  diff_mat %>%
+    as.matrix() %>%
+    as_tibble() %>%
+    rename_with(~ as.character(seq_len(index_n))) %>%
+    mutate(Species = Occ_mod$sp.names) %>%
+    pivot_longer(
+      -Species,
+      names_to = index_name,
+      values_to = "Diff.fit"
+    ) %>%
+    mutate("{index_name}" := as.integer(.data[[index_name]]))
+}
+  
+# Generate tibbles
+occ.covs2 <- msOcc_l$occ.covs %>% 
+  unite(Id_muestreo_ano, all_of(Row_identifier), sep = ".", remove = FALSE) 
+Id_muestreo_ano <- occ.covs2 %>% pull(Id_muestreo_ano)
+Site_ids <- tibble(Site = 1:length(Id_muestreo_ano), Id_muestreo_ano)
+
+n.sites <- dim(msOcc_l$y)[2]
+n.reps <- dim(msOcc_l$y)[3]
+diff_tbl_site <- ppc_to_tbl(diff_mat = diff_q50_site, 
+                            index_name = "Site", 
+                            index_n = n.sites) %>% 
+  left_join(Site_ids)
+diff_tbl_rep <- ppc_to_tbl(diff_mat = diff_q50_rep, 
+                           index_name = "Rep", 
+                           index_n = n.reps) 
+nrow(diff_tbl_rep) # One row per species X site combo so cant match up with Event_covs easily 
+
+# Plot - Site
+diff_tbl_site %>% ggplot() + 
+  geom_point(aes(x = Site, y = Diff.fit, color = Species)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  guides(color = "none") +
+  labs(y = "Replicate - True Discrepancy")
+# Plot - Replicate
+diff_tbl_rep %>% ggplot() + 
+  geom_point(aes(x = Rep, y = Diff.fit, color = Species)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  guides(color = "none") +
+  labs(y = "Replicate - True Discrepancy")
+
+# Identify sites and species to investigate
+Prob_sites <- diff_tbl_site %>% filter(abs(Diff.fit) > 1)
+occ.covs2 %>% semi_join(Prob_sites) %>% view()
+
+# Plot lack of fit by covariates
+Site_covs2 %>% 
+  mutate(Id_muestreo_ano = paste(Id_muestreo, Ano_grp, Season, sep = ".")) %>%
+  left_join(diff_tbl_site) %>%
+  #filter(Diff.fit < -1.5) %>%
+  ggplot(aes(x = Uniq_db, y = Diff.fit, color = Ecoregion)) + 
+  #geom_boxplot() + 
+  geom_jitter(width = .1, height = .04, alpha = .5) + 
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  labs(y = "Replicate - True Discrepancy")
+
+# Which species are problematic?
+occ.covs2 %>% left_join(diff_tbl_site) %>% 
+  filter(Uniq_db == "Gaica mbd" & Diff.fit < -1.5) %>% 
+  distinct(Id_muestreo, Ano1, Species, Diff.fit) %>% 
+  count(Species, sort = T)
+
+Abund_no_obs2 %>% filter(Species_ayerbe_ == "Myiozetetes_cayanensis") %>% 
+  count(Rep_season, sort = T)
+  #tabyl(Id_muestreo)
+
+diff_tbl_rep %>% filter(abs(Diff.fit) > 2) %>% 
+  arrange(Rep) %>% view()
+parms_df %>% filter(Species_num == "Formicivora_grisea")
 
 # >>Traceplots ------------------------------------------------------------
 # Spatial parameters
@@ -331,6 +434,11 @@ plot(Occ_mod, param = "theta", density = FALSE)
 # Traceplots - factor loadings 
 #plot(Occ_mod$lambda.samples[,140:150], param = "lambda", density = FALSE)
 plot(Occ_mod, param = "lambda", density = FALSE)
+
+# Performs a t-test between the first and last part of a Markov chain. If the samples are drawn from the stationary posterior distribution, then the means should be essentially equal and the returned z-score should be between approximately 2 and -2.
+z_geweke <- geweke.diag(Occ_mod$lambda.samples)$z
+mean(abs(z_geweke), na.rm = TRUE)
+#geweke.plot(Occ_mod$lambda.samples)
 
 # >>Inspect factor loadings ------------------------------------------------
 # Lambda samples are an mcmc object with dimensions of number of samples x (spp * number of factors). 
@@ -350,19 +458,6 @@ sp_fac_tbl %>% mutate(abs_val = abs(value)) %>%
   slice_max(abs_val, by = factor) 
 
 # >Inspect parm estimates -------------------------------------------------
-# Species-specific effects 
-parms_df %>% 
-  filter(parameter == "Theta" & term == "phi") %>% # & term == "phi" sigma.sq
-  plot_parm_estimates(ms = TRUE)
-
-parms_df %>% filter(parameter == "Beta") %>% 
-  filter(str_detect(term, "Habitat")) %>%
-  plot_parm_estimates(ms = TRUE)
-
-parms_df %>% filter(parameter == "Alpha") %>% 
-  filter(str_detect(term, "Prob_breed")) %>%
-  plot_parm_estimates(ms = TRUE)
-
 # Community level effects
 draws_beta <- as_draws_df(Occ_mod$beta.comm.samples) %>% 
   pivot_longer(cols = everything(), names_to = "param", values_to = "value")
@@ -382,16 +477,17 @@ plot_half_eye <- function(df){
 # Understand what is contained in (Intercept)
 draws_beta %>% 
   #filter(str_detect(param, "Habitat")) %>%
-  filter(!str_detect(param, "Ecoregion|Intercept")) %>% 
-  plot_half_eye() + 
-  labs(title = "100 species")
+  #filter(!str_detect(param, "Ecoregion|Intercept")) %>% 
+  plot_half_eye() #+ 
+  #labs(title = "100 species")
+plogis(.3)
 draws_alpha %>% 
-  filter(!str_detect(param, "institucion|Intercept")) %>%
+  #filter(!str_detect(param, "institucion|Intercept")) %>%
   plot_half_eye() + 
   labs(title = "100 species")
+plogis(-2) # Low detection probability
 
 #ggsave("Figures/alpha_post_8fac.png", bg = "white") #beta
-
 # Can play with bayesplot::mcmc_dens() function as well, or as.data.frame() to get posterior draws 
 
 # >z samples --------------------------------------------------------------
@@ -407,9 +503,6 @@ sum(Occ_mod$z.samples[4,,2]) # Species richness at site 2 in posterior sample 4
 richness_tbl <- Occ_mod$z.samples %>%
   apply(c(1,3), sum) %>%
   as_tibble()
-occ.covs2 <- msOcc_l$occ.covs %>% 
-  unite(Id_muestreo_ano, all_of(Row_identifier), sep = ".", remove = FALSE) 
-Id_muestreo_ano <- occ.covs2 %>% pull(Id_muestreo_ano)
 richness_tbl %>% 
   rename_with(.cols = everything(), ~ Id_muestreo_ano) %>%
   mutate(sample = row_number()) %>%
